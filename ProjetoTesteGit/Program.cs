388b079e56fecb186cb4944d4b987fe7a21d2a2e
Paulo Jr <juuninho@gmail.com>
2021-06-25 09:37:21 -0300

    Projeto iniciado

---
+++ ProjetoTesteGit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ola Git!");
+            Console.ReadLine();
+        }
+    }
+}
